721f0024273cbdae124503879a27191f34f88180
Nguyen Duc Vinh <vinhb2014631@student.ctu.edu.vn>
2023-10-03 20:24:54 +0700

    From Th1

---
+++ app/routes/contact.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const contacts = require("../controller/contact.controller");
+
+const router = express.Router();
+
+router.get("/", contacts.findAll);
+
+router.post("/", contacts.create);
+
+router.delete("/",contacts.deleteAll);
+
+router.get("/favorite", contacts.findAllFavorite);
+
+router.get("/:id", contacts.findOne);
+
+router.put("/:id", contacts.update);
+
+router.delete("/:id", contacts.delete);
+
+module.exports = router;
