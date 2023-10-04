c9cf39caa7ad5285a35f22274fd64dc88c99914c
Nguyen Duc Vinh <vinhb2014631@student.ctu.edu.vn>
2023-10-04 15:16:40 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controller/contact.controller.js
+++ app/controller/contact.controller.js
@@ -1,27 +1,156 @@
-exports.create = (req, res) => {
-    res.send({message: "create handler"});
+
+const ApiError = require("../api-error")
+const ContactService = require("../services/contact.service")
+const MongoDB = require("../untils/mongodb.untils")
+
+exports.create = async (req, res, next) => {
+    console.log(req.body)
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name can't be empty"))
+    }
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const document = await contactService.create(req.body)
+        return res.send(document)
+    }
+    catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
 }
 
-exports.findAll = (req, res) => {
-    res.send({message: "findAll handler"});
+exports.findAll = async (req, res, next) => {
+    let documents = []
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const { name } = req.query
+        
+        if (name) {
+            documents = await contactService.findByName(name)
+        }
+        else {
+            documents = await contactService.find()
+        }
+
+    }
+
+    catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
+
+    res.send(documents)
 }
 
-exports.findOne = (req, res) => {
-    res.send({message: "findOne handler"});
+exports.findOne = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client) 
+        const documents = await contactService.findByID(req.params.id)
+
+        if (!documents) {
+            return next (
+                new ApiError(400, "Contact not found")
+            )
+        }
+        console.log(documents)
+        
+        return res.send(documents)
+    }
+
+    catch (error) {
+        return next (
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
 }
 
-exports.update = (req, res) => {
-    res.send({message: "update handler"});
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        return next (
+            new ApiError(400, "Data can't be empty")
+        )
+    }
+
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const documents = await contactService.update(req.params.id, req.body)
+
+        if (!documents) {
+            return next (
+                new ApiError(400, "Contact not found")
+            )
+        }
+
+        return res.send({message: "Contact was updated successfully!"})
+        
+    }
+
+    catch (error) {
+        return next (
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
 }
 
-exports.delete = (req, res) => {
-    res.send({message: "delete handler"});
+exports.delete = async (req, res, next) => {
+    if (Object.keys(req.body).length == 0) {
+        return next (
+            new ApiError(400, "Data can't be empty")
+        )
+    }
+
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const document = await contactService.delete(req.params.id)
+
+        if (!document) {
+            return next (
+                new ApiError(400, "Contact not found")
+            )
+        }
+
+        return res.send(document)
+
+    }
+
+    catch (error) {
+        return next (
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
 }
 
-exports.deleteAll = (req, res) => {
-    res.send({message: "deleteAll handler"});
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const coutDelete = await contactService.deleteAll()
+
+        return res.send({
+            message: `${coutDelete} deleted!`
+        })
+    }
+
+    catch (error) {
+        return next (
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
+    
 }
 
-exports.findAllFavorite = (req, res) => {
-    res.send({message: "findAllFavorite handler"});
+exports.findAllFavorite = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client)
+        const documents = await contactService.findAllFavorite(); 
+        
+        return res.send(documents)
+    }
+
+    catch (error) {
+        return next (
+            new ApiError(500, "An error occurred while creating the contact")
+        )
+    }
 }
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const app = require("./app");
+const { log } = require('console')
+const app = require('./app')
+const config = require('./app/config')
+const MongoDB = require('./app/untils/mongodb.untils')
 
-const config = require("./app/config");
+const port = config.app.port
+log(config.db.uri)
 
-// Start server
-const PORT = config.app.port;
+async function startServer () {
+    try {
+        await MongoDB.connect(config.db.uri)
+        log('Connected!')
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+        app.listen(port, () => {
+            log(`Server is running on port ${port}`)
+        })
+    }
+    catch (error) {
+        log("Can't connect to database", error)
+        process.exit()
+    }
+}
+startServer()
